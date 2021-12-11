6e661cbe64b15f39d45370023abfbcfa41e64adc
Nana <alfreeanaalfie@gmail.com>
2021-12-11 23:03:03 +0800

    update payment request designs

---
+++ .history/lib/pages/all_request_20211211223734.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_banking_app/constant/string.dart';
+import 'package:flutter_banking_app/utils/styles.dart';
+import 'package:flutter_banking_app/widgets/my_app_bar.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:http/http.dart' as http;
+
+class UsersPage extends StatefulWidget {
+  const UsersPage({Key? key}) : super(key: key);
+
+  @override
+  _UsersPageState createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  // late Map<String, dynamic> userMap;
+  // late Map<String, dynamic> verifyMap;
+  // List<User> userList = [];
+
+  // Future getList() async {
+  //   final response = await http.get(
+  //     getUserList,
+  //     headers: Values.headers,
+  //   );
+
+  //   if (response.statusCode == 200) {
+  //     verifyMap = jsonDecode(response.body);
+
+  //     var verifyData = Verify.fromJSON(verifyMap);
+
+  //     if (verifyData.status == ApiSTR.successTxt) {
+  //       userMap = jsonDecode(response.body);
+  //       for (var userData in userMap['data']) {
+  //         final users = User.fromMap(userData);
+
+  //         setState(() {
+  //           userList.add(users);
+  //         });
+  //       }
+  //     }
+  //   }
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   getList();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: myAppBar(
+          title: Str.newRequestTxt, implyLeading: true, context: context),
+      // drawer: SideDrawer(),
+      backgroundColor: Styles.primaryColor,
+      body: ExpandableTheme(
+        data: const ExpandableThemeData(
+          iconColor: Colors.blue,
+          useInkWell: true,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: ListView(
+            physics: const BouncingScrollPhysics(),
+            children: <Widget>[
+              Card1
+              // for (User user in userList) Card1(users: user),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ .history/lib/utils/styles_20211211224848.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_banking_app/utils/size_config.dart';
+
+class Styles {
+  static Color primaryColor = const Color(0xFF161D28);
+  static Color primaryWithOpacityColor = const Color(0xFF212E3E);
+  static Color yellowColor = const Color(0xFFDFE94B);
+  static Color accentColor = const Color(0xFF024751);
+  static Color greyColor = const Color(0xFFDFE1E3);
+  static Color buttonColor = const Color(0xFF4C66EE);
+  static Color blueColor = const Color(0xFF4BACF7);
+  static TextStyle textStyle =
+      TextStyle(fontSize: getProportionateScreenWidth(15));
+  static TextStyle titleStyle = TextStyle(
+      fontFamily: 'DMSans',
+      fontSize: getProportionateScreenWidth(19),
+      fontWeight: FontWeight.w500);
+  static const TextStyle subtitleStyle =
+      TextStyle(
+        fontWeight: FontWeight.bold, 
+        fontSize: 21, 
+        color: Colors.white);
+  static const TextStyle subtitleStyle02 =
+      TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold);
+  static TextStyle subtitleStyle03 =
+      TextStyle(
+        color: const Color.fromRGBO(255, 255, 255, 0.7), 
+        fontSize: getProportionateScreenWidth(18), 
+        fontWeight: FontWeight.bold
+      );
+
+  static TextStyle subtitleStyleDark =
+      TextStyle(
+        fontWeight: FontWeight.bold, 
+        fontSize: getProportionateScreenWidth(21), 
+        color: Colors.black);
+  static TextStyle subtitleStyleDark02 =
+      TextStyle(
+        color: Colors.black, 
+        fontSize: 18, 
+        fontWeight: FontWeight.bold);
+  static TextStyle subtitleStyleDark03 =
+      TextStyle(
+        color: const Color.fromRGBO(0, 0, 0, 0.7), 
+        fontSize: getProportionateScreenWidth(18), 
+        fontWeight: FontWeight.bold
+      );
+
+  static const TextStyle cardDetails =
+      TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.normal);
+  static const TextStyle cardTitle =
+      TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold);
+
+  static const Color whiteColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color secondaryColor = Color.fromRGBO(0, 122, 254, 1);
+  static const Color textColor = Color.fromRGBO(33, 34, 42, 1);
+  static const Color darkBlueColor = Color.fromRGBO(29, 39, 68, 1);
+  static const Color backgroundColor = Color.fromRGBO(242, 242, 242, 1);
+
+  static const Color dangerColor = Color.fromRGBO(245, 54, 92, 1);
+  static const Color warningColor = Color.fromRGBO(251, 99, 64, 1);
+  static const Color successColor = Color.fromRGBO(45, 206, 137, 1);
+  static const Color infoColor = Color.fromRGBO(17, 205, 239, 1);
+
+  static const Color transparentColor = Color.fromRGBO(0, 0, 0, 0);
+}
