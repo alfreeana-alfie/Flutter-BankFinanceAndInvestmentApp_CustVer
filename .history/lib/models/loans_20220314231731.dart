015ad304024f9badab3e6c36a71ee1eea1437758
Nana <alfreeanaalfie@gmail.com>
2022-03-14 23:53:00 +0800

    add loan list and update loan functions

---
+++ .history/lib/models/loans_20220314231731.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_banking_app/utils/string.dart';
+
+class Loan {
+  int?  id;
+  String? borrowerId, approvedUserId, createdUserId, branchId, status,loanId,
+      loanProductId,
+      firstPaymentDate,
+      releaseDate,
+      currencyId,
+      appliedAmount,
+      totalPayable,
+      totalPaid,
+      latePaymentPenalty,
+      attachment,
+      nationalIdentityCard,
+      bankStatement,
+      description,
+      remarks,
+      approvedDate,
+      createdAt,
+      transactionCode;
+
+  Loan(
+      {this.id,
+      this.loanId,
+      this.loanProductId,
+      this.borrowerId,
+      this.firstPaymentDate,
+      this.releaseDate,
+      this.currencyId,
+      this.appliedAmount,
+      this.totalPayable,
+      this.totalPaid,
+      this.latePaymentPenalty,
+      this.attachment,
+      this.nationalIdentityCard,
+      this.bankStatement,
+      this.description,
+      this.remarks,
+      this.status,
+      this.approvedDate,
+      this.approvedUserId,
+      this.createdUserId,
+      this.branchId,
+      this.createdAt,
+      this.transactionCode, });
+
+  factory Loan.fromMap(Map<String, dynamic> map) {
+    return Loan(
+      // id: map[Field.id] as int?,
+      loanId: map[Field.loanId] as String?,
+      borrowerId: map[Field.borrowerId] as String?,
+      firstPaymentDate: map[Field.firstPaymentDate] as String?,
+      releaseDate: map[Field.releaseDate] as String?,
+      appliedAmount: map[Field.appliedAmount] as String?,
+      totalPayable: map[Field.totalPayable] as String?,
+      totalPaid: map[Field.totalPaid] as String?,
+      latePaymentPenalty: map[Field.latePaymentPenalty] as String?,
+      attachment: map[Field.attachment] as String?,
+      nationalIdentityCard: map[Field.nationalIdentityCard] as String?,
+      bankStatement: map[Field.bankStatement] as String?,
+      description: map[Field.description] as String?,
+      remarks: map[Field.remarks] as String?,
+      status: map[Field.status] as String?,
+      approvedUserId: map[Field.approvedUserId] as String?,
+      createdUserId: map[Field.createdUserId] as String?,
+      branchId: map[Field.branchId] as String?,
+      createdAt: map[Field.createdAt] as String?,
+      transactionCode: map[Field.transactionCode] as String?,
+    );
+  }
+}
---
+++ .history/lib/widgets/transaction_today_admin_20220314225614.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_banking_app/methods/config.dart';
+import 'package:flutter_banking_app/models/wallet_transaction.dart';
+import 'package:flutter_banking_app/utils/api.dart';
+import 'package:flutter_banking_app/utils/string.dart';
+import 'package:flutter_banking_app/utils/styles.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+class TransactionTodayListAdmin extends StatefulWidget {
+  const TransactionTodayListAdmin({Key? key}) : super(key: key);
+
+  @override
+  _TransactionTodayListAdminState createState() =>
+      _TransactionTodayListAdminState();
+}
+
+class _TransactionTodayListAdminState extends State<TransactionTodayListAdmin> {
+  SharedPref sharedPref = SharedPref();
+  List<WalletTransaction> transactionListNew = [];
+
+  void getCurrency() async {
+    final response =
+        await http.get(AdminAPI.listOfWalletTransactions, headers: headers);
+
+    if (response.statusCode == Status.ok) {
+      var jsonBody = jsonDecode(response.body);
+
+      for (var data in jsonBody[Field.data]) {
+        final transaction = WalletTransaction.fromMap(data);
+
+        // print(transaction.amount);
+
+        setState(() {
+          transactionListNew.add(transaction);
+        });
+      }
+    } else {
+      print(Status.failed);
+    }
+  }
+
+  @override
+  void initState() {
+    getCurrency();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      itemCount: transactionListNew.length,
+      itemBuilder: (BuildContext context, int i) {
+        // final trs = transactions[i];
+
+        String? method;
+
+        switch (transactionListNew[i].method) {
+          case 'send_money':
+            method = 'Send Money';
+            break;
+
+          case 'exchange_money':
+            method = 'Exchange Money';
+            break;
+
+          case 'wire_transfer':
+            method = 'Wire Transfer';
+            break;
+
+          case 'top_up_account':
+            method = 'Top Up by Accountant';
+            break;
+
+          case 'top_up':
+            method = 'Top Up';
+            break;
+
+          case 'sms_subcribed':
+            method = 'SMS Subcription';
+            break;
+
+          
+          default:
+        }
+
+        DateTime tempDate =
+            DateTime.parse(transactionListNew[i].createdAt ?? '-');
+        String createdAt = DateFormat('dd-MM-yyyy hh:mm aa').format(tempDate);
+
+        return ListTile(
+          isThreeLine: true,
+          minLeadingWidth: 10,
+          contentPadding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          title: Text(
+              '${transactionListNew[i].userName!.toUpperCase()} - $method',
+              style: const TextStyle(color: Styles.textColor)),
+          subtitle: Text(createdAt,
+              style: TextStyle(color: Styles.textColor.withOpacity(0.5))),
+          trailing: Text(
+              'NGN ${double.parse(transactionListNew[i].amount ?? '0').toStringAsFixed(2)}',
+              style: const TextStyle(fontSize: 17, color: Styles.textColor)),
+        );
+      },
+    );
+  }
+}
